81011c349d22c271cf788b2a45c5942a71e23cd6
MoleOrbitalHybridAnalyst <lch004218@gmail.com>
2018-02-06 18:47:49 -0600

    add to_colvar method to DataFrame

--- read_colvar.py
+++ read_colvar.py
@@ -10,3 +10,15 @@ def read_colvar(fname):
         raise NameError('No header found in ' + fname)
     names_ = firstline.split()[2:]
     return pd.read_csv(fname,delim_whitespace=True,names=names_, comment ='#' )
+
+def to_colvar(df, fname):
+    with open(fname, "w") as fp:
+        header = "#! FIELDS"
+        for _ in df.columns:
+            header += " " + _
+        print(header, file=fp)
+        for index, row in df.iterrows():
+            [fp.write(" %8f"%row[_]) for _ in df.columns]
+            print("", file=fp)
+
+setattr(pd.DataFrame, "to_colvar", to_colvar)
