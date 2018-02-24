b7dde6f261fb5ef89ca6aaa439d66ba35e33d1c8
MoleOrbitHybridAnalyst <lch004218@gmail.com>
2018-02-23 22:28:58 -0600

    add pdb2xyz

---
+++ make_pdb2xyz.sh
@@ -0,0 +1,1 @@
+g++ pdb2xyz.cpp -o pdb2xyz -lpdb -std=c++0x -L/home/chhli/local/lib -I/home/chhli/share/pdb -Wall
---
+++ pdb2xyz.cpp
@@ -0,0 +1,22 @@
+// ./pdb2xyz in.pdb out.xyz 
+// ~/share/*def needed to determine groups
+// assume no H3O as this is gmx
+#include <iostream>
+#include <string>
+#include <exception>
+#include <cstdio>
+#include <cstdlib>
+#include <unistd.h>
+#include "pdb.h"
+using namespace std;
+using namespace PDB_NS;
+
+int main(int argc, char **argv) 
+{
+   PDB pdb(argv[1]);
+
+   pdb.writeIndexFile(argv[2],"system");
+
+   return 0;
+
+}
