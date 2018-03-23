a8d20db48e6235b681912dc975e821bf9e8fd8b5
MoleOrbitalHybridAnalyst <lch004218@gmail.com>
2018-03-23 18:47:02 -0500

    refine print_colvar.py

--- print_colvar.py
+++ print_colvar.py
@@ -15,20 +15,26 @@ def parse():
 def main():
     args = parse()
 
-    df = read_colvar(args.COLVAR)
+    try:
+        df = read_colvar(args.COLVAR)
+    except FileNotFoundError as fnferr:
+        print(args.COLVAR + ' does not exist', file=sys.stderr); exit()
+    except RuntimeError as rterr:
+        print(rterr, file=sys.stderr); exit()
 
     """
-    if --fields is absent, print out all the available fields
-    if --regex is absent, directly use the input fields to indexing the df
-    if --regex is present, use re to find matching fileds, use unique to remove
-        duplicates, use indexes returned by unique to recover the orginal 
-        order of the fields
-    if no matching fields, throw a KeyError exception
+        if --fields is absent, print out all the available fields
+        if --regex is absent, directly use the input fields to indexing the df
+        if --regex is present, use re to find matching fileds, use unique to 
+            remove duplicates, use indexes returned by unique to recover the 
+            orginal order of the fields
+        if no matching fields, throw a KeyError exception
     """
 
     if args.fields is None: 
         df.print_columns()
     else:
+        # get the fields
         raw_f = args.fields.split(',')
         if not args.regex: result_f = raw_f
         else:
@@ -37,6 +43,8 @@ def main():
                 [col for f in raw_f for col in df.columns if re.match(f,col)])
             indexes = np.unique(result_f, return_index=True)[1]
             result_f = result_f[sorted(indexes)]
+
+        # update df using the fields
         try: 
             if not len(result_f): 
                 raise KeyError("no matching fields with " + args.fields)
@@ -46,12 +54,15 @@ def main():
             print("available fields are:", file=sys.stderr)
             df.print_columns(sys.stderr); exit()
 
-        if not args.noheader: df.print_header()
-        df.print_colvar()
+        # print df
+        try:
+            if not args.noheader: df.print_header()
+            df.print_colvar()
+        except BrokenPipeError:
+            pass
+
+        # suppress the 'exception ignored' message
+        sys.stderr.close()
 
 if __name__=='__main__':
-    try:
-        main()
-    except BrokenPipeError:
-        pass
-    sys.stderr.close()
+    main()
--- read_colvar.py
+++ read_colvar.py
@@ -8,7 +8,7 @@ def read_colvar(fname):
     with open(fname, "r") as fp:
         firstline = fp.readline()
     if re.match("#!.+FIELDS", firstline) == None:
-        raise NameError('No header found in ' + fname)
+        raise RuntimeError('No header found in ' + fname)
     names_ = firstline.split()[2:]
     return pd.read_csv(fname,delim_whitespace=True,names=names_, comment ='#' )
 
