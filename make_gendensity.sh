c293dc4a19fdd3e82ab45bfc2fd1f2bdab2da353
MoleOrbitalHybridAnalyst <lch004218@gmail.com>
2018-05-09 21:07:06 -0500

    update make_gendensity.sh to compile with mpi

--- make_gendensity.sh
+++ make_gendensity.sh
@@ -1,2 +1,4 @@
 #g++ gendensity.cpp -o gendensity -lpdb -std=c++0x -L/home/chhli/local/lib -I/home/chhli/share/pdb -Wall
-g++ gendensity.cpp -o gendensity -lpdb -L/home/chhli/local/lib -I/home/chhli/share/pdb/include -Wall
+#g++ gendensity.cpp -o gendensity -lpdb -L/home/chhli/local/lib -I/home/chhli/share/pdb/include -Wall
+module load intelmpi/2017.up4+intel-17.0 gcc/6.1
+mpicxx gendensity.cpp -o gendensity -lpdb -L/home/chhli/local/lib -I/home/chhli/share/pdb/include -Wall
