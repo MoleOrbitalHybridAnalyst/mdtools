c7fafb9afc6117811bf7d6cd6a5223b6260cf628
MoleOrbitalHybridAnalyst <lch004218@gmail.com>
2018-07-31 10:50:18 -0500

    enable to do spectrum without total dipole

--- spec.py
+++ spec.py
@@ -3,6 +3,7 @@ from autocorrelate import *
 from scipy import fftpack
 
 boxsize = 19.3887
+do_dipole = False
 
 def pbc(x):
     n = (x + 0.5 * boxsize) // boxsize
@@ -18,22 +19,24 @@ def do_spec(string):
     vx = np.vectorize(pbc)(vx)
     vy = np.vectorize(pbc)(vy)
     vz = np.vectorize(pbc)(vz)
-    dx = df_dipole.dx.values[:len(x)] 
-    dy = df_dipole.dy.values[:len(x)]
-    dz = df_dipole.dz.values[:len(x)]
-    vdx = dx[2:] - dx[:-2]
-    vdy = dy[2:] - dy[:-2]
-    vdz = dz[2:] - dz[:-2]
-    vdx = np.vectorize(pbc)(vdx)
-    vdy = np.vectorize(pbc)(vdy)
-    vdz = np.vectorize(pbc)(vdz)
+    if do_dipole:
+        dx = df_dipole.dx.values[:len(x)] 
+        dy = df_dipole.dy.values[:len(x)]
+        dz = df_dipole.dz.values[:len(x)]
+        vdx = dx[2:] - dx[:-2]
+        vdy = dy[2:] - dy[:-2]
+        vdz = dz[2:] - dz[:-2]
+        vdx = np.vectorize(pbc)(vdx)
+        vdy = np.vectorize(pbc)(vdy)
+        vdz = np.vectorize(pbc)(vdz)
     vc = autocorrelate(vx) + autocorrelate(vy) + autocorrelate(vz)
-    vc_cross = \
-    correlate(vx, vdx-vx) + correlate(vy, vdy-vy) + correlate(vz, vdz-vz) + \
-    correlate(vdx-vx, vx) + correlate(vdy-vy, vy) + correlate(vdz-vz, vz)
-    vc_rest = autocorrelate(vdx - vx) + \
-              autocorrelate(vdy - vy) + \
-              autocorrelate(vdz - vz) 
+    if do_dipole:
+        vc_cross = \
+        correlate(vx, vdx-vx) + correlate(vy, vdy-vy) + correlate(vz, vdz-vz) + \
+        correlate(vdx-vx, vx) + correlate(vdy-vy, vy) + correlate(vdz-vz, vz)
+        vc_rest = autocorrelate(vdx - vx) + \
+                  autocorrelate(vdy - vy) + \
+                  autocorrelate(vdz - vz) 
     n = int(len(vc) * 0.01)
     #vf = np.real(np.fft.fft(vc[:n]))
     #vf_cross = np.real(np.fft.fft(vc_cross[:n]))
@@ -41,40 +44,66 @@ def do_spec(string):
     #freq = np.fft.fftfreq(n, d = 0.5)
     #wn = 33356.40952 * freq
     vf = fftpack.dct(vc[:n])
-    vf_cross = fftpack.dct(vc_cross[:n])
-    vf_rest = fftpack.dct(vc_rest[:n])
+    if do_dipole:
+        vf_cross = fftpack.dct(vc_cross[:n])
+        vf_rest = fftpack.dct(vc_rest[:n])
     freq = np.fft.fftfreq(n, d = 0.5)
     wn = 33356.40952 * freq / 2.0
-    return [vc, wn, vf, vf_cross, vf_rest]
+    if do_dipole:
+        return [vc, wn, vf, vf_cross, vf_rest]
+    else:
+        return [vc, wn, vf]
 
 df = read_colvar("COLVAR")
-df_dipole = read_colvar("dipole.dat")
+if do_dipole:
+    df_dipole = read_colvar("dipole.dat")
 
-[c_rmdcec, wn_rmdcec, f_rmdcec, f_cross_rmdcec, f_rest_rmdcec] \
-        = do_spec("pos_rmdcec")
-[c_qc, wn_qc, f_qc, f_cross_qc, f_rest_qc] = do_spec("pos_qc")
-[c_pi, wn_pi, f_pi, f_cross_pi, f_rest_pi] = do_spec("pos_pi")
-[c_evbcec, wn_evbcec, f_evbcec, f_cross_evbcec, f_rest_evbcec] \
-        = do_spec("pos_evbcec")
+if do_dipole:
+    [c_rmdcec, wn_rmdcec, f_rmdcec, f_cross_rmdcec, f_rest_rmdcec] \
+            = do_spec("pos_rmdcec")
+    [c_qc, wn_qc, f_qc, f_cross_qc, f_rest_qc] = do_spec("pos_qc")
+    [c_pi, wn_pi, f_pi, f_cross_pi, f_rest_pi] = do_spec("pos_pi")
+    [c_evbcec, wn_evbcec, f_evbcec, f_cross_evbcec, f_rest_evbcec] \
+            = do_spec("pos_evbcec")
+    
+    
+    df_ = pd.DataFrame([[_,__1,__2,__3,__1+__2,__1+__2+__3] for _,__1,__2,__3 in \
+            zip(wn_rmdcec,f_rmdcec,f_cross_rmdcec,f_rest_rmdcec)])
+    df_.columns = ["wavenumber","abs", "cross", "rest", "abs+cross", "total"]
+    df_[df_.wavenumber>=0].to_colvar("./rmdcec_abs.dat")
+    df_ = pd.DataFrame([[_,__1,__2,__3,__1+__2,__1+__2+__3] for _,__1,__2,__3 in zip(wn_qc,f_qc,f_cross_qc,f_rest_qc)])
+    df_.columns = ["wavenumber","abs", "cross", "rest", "abs+cross", "total"]
+    df_[df_.wavenumber>=0].to_colvar("./qc_abs.dat")
+    df_ = pd.DataFrame([[_,__1,__2,__3,__1+__2,__1+__2+__3] for _,__1,__2,__3 in zip(wn_pi,f_pi,f_cross_pi,f_rest_pi)])
+    df_.columns = ["wavenumber","abs", "cross", "rest", "abs+cross", "total"]
+    df_[df_.wavenumber>=0].to_colvar("./pi_abs.dat")
+    df_ = pd.DataFrame([[_,__1,__2,__3,__1+__2,__1+__2+__3] for _,__1,__2,__3 in \
+            zip(wn_evbcec,f_evbcec,f_cross_evbcec,f_rest_evbcec)])
+    df_.columns = ["wavenumber","abs", "cross", "rest", "abs+cross", "total"]
+    df_[df_.wavenumber>=0].to_colvar("./evbcec_abs.dat")
 
-
-df_ = pd.DataFrame([[_,__1,__2,__3,__1+__2,__1+__2+__3] for _,__1,__2,__3 in \
-        zip(wn_rmdcec,f_rmdcec,f_cross_rmdcec,f_rest_rmdcec)])
-df_.columns = ["wavenumber","abs", "cross", "rest", "abs+cross", "total"]
-df_[df_.wavenumber>=0].to_colvar("./rmdcec_abs.dat")
-df_ = pd.DataFrame([[_,__1,__2,__3,__1+__2,__1+__2+__3] for _,__1,__2,__3 in zip(wn_qc,f_qc,f_cross_qc,f_rest_qc)])
-df_.columns = ["wavenumber","abs", "cross", "rest", "abs+cross", "total"]
-df_[df_.wavenumber>=0].to_colvar("./qc_abs.dat")
-df_ = pd.DataFrame([[_,__1,__2,__3,__1+__2,__1+__2+__3] for _,__1,__2,__3 in zip(wn_pi,f_pi,f_cross_pi,f_rest_pi)])
-df_.columns = ["wavenumber","abs", "cross", "rest", "abs+cross", "total"]
-df_[df_.wavenumber>=0].to_colvar("./pi_abs.dat")
-df_ = pd.DataFrame([[_,__1,__2,__3,__1+__2,__1+__2+__3] for _,__1,__2,__3 in \
-        zip(wn_evbcec,f_evbcec,f_cross_evbcec,f_rest_evbcec)])
-df_.columns = ["wavenumber","abs", "cross", "rest", "abs+cross", "total"]
-df_[df_.wavenumber>=0].to_colvar("./evbcec_abs.dat")
+else:
+    [c_rmdcec, wn_rmdcec, f_rmdcec] = do_spec("pos_rmdcec")
+    [c_qc, wn_qc, f_qc] = do_spec("pos_qc")
+    [c_pi, wn_pi, f_pi] = do_spec("pos_pi")
+    [c_evbcec, wn_evbcec, f_evbcec] = do_spec("pos_evbcec")
+    
+    
+    df_ = pd.DataFrame([[_,__1] for _,__1 in zip(wn_rmdcec,f_rmdcec)])
+    df_.columns = ["wavenumber","abs"]
+    df_[df_.wavenumber>=0].to_colvar("./rmdcec_abs.dat")
+    df_ = pd.DataFrame([[_,__1] for _,__1 in zip(wn_qc,f_qc)])
+    df_.columns = ["wavenumber","abs"]
+    df_[df_.wavenumber>=0].to_colvar("./qc_abs.dat")
+    df_ = pd.DataFrame([[_,__1] for _,__1 in zip(wn_pi,f_pi)])
+    df_.columns = ["wavenumber","abs"]
+    df_[df_.wavenumber>=0].to_colvar("./pi_abs.dat")
+    df_ = pd.DataFrame([[_,__1] for _,__1 in zip(wn_evbcec,f_evbcec)])
+    df_.columns = ["wavenumber","abs"]
+    df_[df_.wavenumber>=0].to_colvar("./evbcec_abs.dat")
 
 #mask = (wn_rmdcec >= 600) & (wn_rmdcec <= 4000)
-mask = wn_rmdcec >= 0
+#mask = wn_rmdcec >= 0
 #plt.figure()
 #plt.plot(wn_rmdcec[mask], f_rmdcec[mask])
 #plt.plot(wn_qc[mask], f_qc[mask])
