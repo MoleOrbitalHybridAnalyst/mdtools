91ba5eb74f33da28aad374e77cbd44e57fb2a0e9
Chenghan Li <lch004218@gmail.com>
2018-08-14 17:16:32 -0500

    add pepthom.tcl

---
+++ pepthom.tcl
@@ -0,0 +1,64 @@
+mol representation DynamicBonds 1.6 0.1 12.0
+mol representation VDW 0.2 12.0
+mol representation NewCartoon 0.25 10.00 1.8 0
+mol showrep 0 0 off
+mol addrep 0
+mol modselect 1 0 protein
+mol modstyle 1 0 NewCartoon
+mol showrep 0 1 off
+mol addrep 0
+mol modselect 2 0 resid <= 52 and protein
+mol modstyle 2 0 NewCartoon
+mol modcolor 2 0 ColorID 1
+mol addrep 0
+mol modselect 3 0 resid >= 52 and resid <= 88 and protein
+mol modstyle 3 0 NewCartoon
+mol modcolor 3 0 ColorID 2
+mol addrep 0
+mol modselect 4 0 resid >= 89 and resid <= 114 and protein
+mol modstyle 4 0 NewCartoon
+mol modcolor 4 0 ColorID 3
+mol addrep 0
+mol modselect 5 0 resid >= 115 and resid <= 148 and protein
+mol modstyle 5 0 NewCartoon
+mol modcolor 5 0 ColorID 4
+mol addrep 0
+mol modselect 6 0 resid >= 149 and resid <= 183 and protein
+mol modstyle 6 0 NewCartoon
+mol modcolor 6 0 ColorID 5
+mol addrep 0
+mol modselect 7 0 resid >= 184 and resid <= 217 and protein
+mol modstyle 7 0 NewCartoon
+mol modcolor 7 0 ColorID 6
+mol addrep 0
+mol modselect 8 0 resid >= 218 and resid <= 254 and protein
+mol modstyle 8 0 NewCartoon
+mol modcolor 8 0 ColorID 7
+mol addrep 0
+mol modselect 9 0 resid >= 255 and resid <= 284 and protein
+mol modstyle 9 0 NewCartoon
+mol modcolor 9 0 ColorID 8
+mol addrep 0
+mol modselect 10 0 resid >= 285 and resid <= 326 and protein
+mol modstyle 10 0 NewCartoon
+mol modcolor 10 0 ColorID 9
+mol addrep 0
+mol modselect 11 0 resid >= 327 and resid <= 366 and protein
+mol modstyle 11 0 NewCartoon
+mol modcolor 11 0 ColorID 10
+mol addrep 0
+mol modselect 12 0 resid >= 367 and resid <= 397 and protein
+mol modstyle 12 0 NewCartoon
+mol modcolor 12 0 ColorID 11
+mol addrep 0
+mol modselect 13 0 resid >= 398 and resid <= 432 and protein
+mol modstyle 13 0 NewCartoon
+mol modcolor 13 0 ColorID 12
+mol addrep 0
+mol modselect 14 0 resid >= 433 and resid <= 466 and protein
+mol modstyle 14 0 NewCartoon
+mol modcolor 14 0 ColorID 13
+mol addrep 0
+mol modselect 15 0 resid >= 467 and protein
+mol modstyle 15 0 NewCartoon
+mol modcolor 15 0 ColorID 14
