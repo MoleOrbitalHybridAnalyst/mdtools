8be938f8d6ea21411719131b7b0fabac30faff21
Chenghan Li <lch004218@gmail.com>
2018-09-06 15:20:27 -0500

    add water in pepthom_residues.tcl

--- pepthom_residues.tcl
+++ pepthom_residues.tcl
@@ -11,11 +11,10 @@ mol modstyle 2 0 Licorice
 mol addrep 0
 mol modselect 3 0 resid 44 311 79 40 41 and protein
 mol modstyle 3 0 Licorice
-##mol selupdate 3 0 1
-##mol addrep 0
-##mol modselect 4 0 resname H3O or same residue as (name OH2 and ((within 7 of (resid 799 and name OG1)) or (within 5 of (resid 767 and name OG)) or (within 5 of (resid 763 and name OH)) or (within 6 of (resid 908 and name OE1 OE2 OD1 OD2)) or (within 7 of (resid 794 and name NE1)) or (within 7 of (resid 944 and name ND1 NE2))))
-##mol modstyle 4 0 VDW
-##mol selupdate 4 0 1
+mol addrep 0
+mol modselect 4 0 water and within 9 of (protein and resid 33 141)
+mol modstyle 4 0 VDW
+mol selupdate 4 0 1
 ###mol addrep 0
 ###mol modselect 4 0 resname H3O
 ###mol modstyle 4 0 CPK
