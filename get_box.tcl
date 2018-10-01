adff76d21fdab26bfa84672ec6cf71f16eecc66e
MoleOrbitalHybridAnalyst <lch004218@gmail.com>
2018-10-01 10:58:11 -0500

    add get_box.tcl

---
+++ get_box.tcl
@@ -0,0 +1,8 @@
+set nf [molinfo 0 get numframes]
+set fp [open [lindex $argv 0] w]
+for {set i 0} {$i < $nf} {incr i} {
+   animate goto $i
+   set pbc_info [lindex [pbc get] 0]
+   puts "$i [lindex $pbc_info 0] [lindex $pbc_info 1] [lindex $pbc_info 2]"
+}
+close $fp
