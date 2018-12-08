de29909fb02125dd8e17b5ce45865754e78d5876
MoleOrbitalHybridAnalyst <lch004218@gmail.com>
2018-12-08 13:46:49 -0600

    make check_overlap2d.py faster

--- check_overlap2d.py
+++ check_overlap2d.py
@@ -2,6 +2,10 @@ import argparse
 import matplotlib.pyplot as plt
 import numpy as np
 import re
+import time
+from read_colvar import *
+
+check_time = False
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
@@ -16,6 +20,10 @@ if __name__=="__main__":
             default='0.2,0.2')
     parser.add_argument('-d','--drawline',
             help='draw line at this value',default=0.05)
+    parser.add_argument('-xt','--xticks',
+            help='x axis ticks definition')
+    parser.add_argument('-yt','--yticks',
+            help='y axis ticks definition')
     args = parser.parse_args()
 
     c1 = int(args.column.split(',')[0])
@@ -33,20 +41,47 @@ if __name__=="__main__":
     cmap = plt.cm.get_cmap('hsv', 10)
     with open(args.list_dir,"r") as fp:
         idir = 0
+        if check_time:
+            t1 = time.time()
         for line in fp:
-            cv1=[]; cv2=[]
             print("checking "+line, end="")
-            with open(line.rstrip()+"/"+args.filename,"r") as f:
-                for l in f:
-                    if re.match("#",l) != None:
-                        continue
-                    splits = l.split()
-                    cv1.append(float(splits[c1]))
-                    cv2.append(float(splits[c2]))
+            try:
+                #data = np.loadtxt(line.rstrip()+"/"+args.filename, usecols = [c1,c2])
+                data = read_colvar(line.rstrip()+"/"+args.filename).values[:, [c1, c2]]
+                cv1 = data[:, 0]
+                cv2 = data[:, 1]
+                if check_time:
+                    t2 = time.time()
+                    print(t2 - t1)
+                    t1 = t2
+            except:
+                print("WARNING: cannot open colvar in", line, end = '')
+                try:
+                    fp = open(line.rstrip()+"/plumed.dat")
+                    for _line in fp:
+                        if re.match("us:", _line):
+                            m =  re.match(".+\sAT=(.+?)\s", _line)
+                            cens = m.group(1)
+                            x = float(cens.split(",")[0])
+                            y = float(cens.split(",")[1])
+                            plt.text(x, y, line.rstrip(),
+                                horizontalalignment = 'center', 
+                                verticalalignment = 'center')
+                    fp.close()
+                except:
+                    pass
+                continue
             if len(cv1) == 0:
                 continue
-            cv1 = np.array(cv1)
-            cv2 = np.array(cv2)
+            min1 = min(cv1); max1 = max(cv1)
+            min2 = min(cv2); max2 = max(cv2)
+#            n1 = int(round((max1 - min1) / w1))
+#            n2 = int(round((max2 - min2) / w2))
+#            print("min1 = %f max1 = %f min2 = %f max2 = %f"%(min1,max1,min2,max2))
+#            print("n1 = %d n2 = %d"%(n1,n2))
+#            histo,_,_ = np.histogram2d(cv1, cv2, bins = [n1,n2], normed = True)
+#            x = np.arange(min1,max1+w1,w1)[:n1] + w1/2.0
+#            y = np.arange(min2,max2+w2,w2)[:n2] + w2/2.0
             min1 = min(cv1); max1 = max(cv1)
             min2 = min(cv2); max2 = max(cv2)
             extend = (max1 - min1) * r1 /2.0
@@ -65,16 +100,29 @@ if __name__=="__main__":
             histo /= len(cv1)
             x = np.arange(min1,max1+w1,w1)[:n1+1] + w1/2.0
             y = np.arange(min2,max2+w2,w2)[:n2+1] + w2/2.0
-            #cp = plt.contour(x,y,histo,[drawline],colors=[cmap(idir)])
-            cp = plt.contourf(x, y, histo,
-                    [drawline, 1.0],colors=[cmap(idir), 'b'],
-                    alpha = 0.2)
+            if check_time:
+                t2 = time.time()
+                print(t2 - t1)
+                t1 = t2
+#            cp = plt.contourf(x, y, histo.T,
+            cp = plt.contour(x, y, histo,
+                    [drawline],colors=[cmap(idir), 'b'],
+                    alpha = 0.8)
             plt.text(np.mean(x), np.mean(y), line.rstrip(), 
                     color = cmap(idir),
                     horizontalalignment = 'center', 
                     verticalalignment = 'center')
+            if check_time:
+                t2 = time.time()
+                print(t2 - t1)
+                t1 = t2
             #cp.levels = [line.rstrip()]
             #plt.clabel(cp, cp.levels)
             idir = (idir + 1) % 10
 
+    if args.xticks is not None:
+        exec('plt.xticks(' + args.xticks + ')')
+    if args.yticks is not None:
+        exec('plt.yticks(' + args.yticks + ')')
+    plt.grid(color='r', linestyle='-', linewidth=1, which ="both")
     plt.show()
