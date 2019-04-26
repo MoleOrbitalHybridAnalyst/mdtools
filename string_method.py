7416c571553abda2e87cd45dcad8bd65c8544e44
MoleOrbitalHybridAnalyst <lch004218@gmail.com>
2019-04-26 15:55:55 -0500

    add string_method.py

---
+++ string_method.py
@@ -0,0 +1,82 @@
+import numpy as np
+from scipy.interpolate import RectBivariateSpline
+import matplotlib.pyplot as plt
+
+# info about pmf
+free = np.loadtxt("free.dat", usecols = [0,1,2])
+path = np.loadtxt("initial_path.dat", usecols = [0, 1])
+xdim = 200
+zmax = 8
+xscale = 1.0; yscale = 1.0
+# computation settings
+update_ends = False      # if update the end points of string
+nbeads = 200             # number of beads in string
+
+
+ydim = int(len(free) / xdim)
+if abs(ydim  - len(free) / xdim) > 1e-50:
+    print("xdim does not match input data")
+    exit(1)
+
+mask = free[:,2] > zmax
+free[mask, 2] = zmax
+free[:,0] *= xscale; free[:,1] *= yscale
+xmin = min(free[:, 0])
+xmax = max(free[:, 0])
+ymin = min(free[:, 1])
+ymax = max(free[:, 1])
+free = free.reshape(xdim, -1, 3)
+
+dx = free[1,0,0] - free[0,0,0]
+dy = free[0,1,1] - free[0,0,1]
+dfx, dfy = np.gradient(free[:,:,2], dx, dy)
+dfx_ = RectBivariateSpline(free[:,0,0], free[0,:,1], dfx)
+dfy_ = RectBivariateSpline(free[:,0,0], free[0,:,1], dfy)
+
+#def grad_nearest(x, y):
+#    xindx = int((x - xmin) / dx)
+#    yindx = int((y - ymin) / dy)
+#    return [dfx[xindx, yindx], dfy[xindx, yindx]]
+
+def grad(x, y):
+    return np.array([dfx_(x,y)[0,0], dfy_(x,y)[0,0]])
+
+def reparametrize_path(path, nbeads):
+    # reference https://stackoverflow.com/questions/18244305/how-to-redistribute-points-evenly-over-a-curve
+    #equally spaced in arclength
+    N = np.transpose(np.linspace(0,1,nbeads))
+    
+    #number of points on the curve
+    n = len(path)
+    p1 = path[0,:]
+    pend = path[-1,:]
+    last_segment = np.linalg.norm(np.subtract(p1,pend))
+    epsilon= 10*np.finfo(float).eps
+    
+    #IF the two end points are not close enough lets close the curve
+    if last_segment > epsilon*np.linalg.norm(np.amax(abs(path),axis=0)):
+        path = np.vstack((path,p1))
+        nbeads = nbeads + 1
+    else:
+        print('Contour already closed')
+    
+    pt = np.zeros((nbeads,2))
+    
+    #Compute the chordal arclength of each segment.
+    chordlen = (np.sum(np.diff(path,axis=0)**2,axis=1))**(1/2)
+    #Normalize the arclengths to a unit total
+    chordlen = chordlen/np.sum(chordlen)
+    #cumulative arclength
+    cumarc = np.append(0,np.cumsum(chordlen))
+    
+    tbins= np.digitize(N,cumarc) # bin index in which each N is in
+    
+    #catch any problems at the ends
+    tbins[np.where(tbins<=0 | (N<=0))]=1
+    tbins[np.where(tbins >= n | (N >= 1))] = n - 1
+    
+    s = np.divide((N - cumarc[tbins]),chordlen[tbins-1])
+    pt = path[tbins,:] + np.multiply((path[tbins,:] - path[tbins-1,:]),(np.vstack([s]*2)).T)
+    
+    return pt
+
