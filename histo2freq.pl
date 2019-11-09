74e7698904fabbf0858762898a1c348ffba2e281
MoleOrbitalHybridAnalyst <lch004218@gmail.com>
2019-11-09 17:45:49 -0600

    better support pipe

--- histo2freq.pl
+++ histo2freq.pl
@@ -1,14 +1,23 @@
 use strict;
-my @input_files = @ARGV;
+#my @input_files = @ARGV;
 my $total = 0;
+my @x = qw{};
+my @y = qw{};
 while(<>) {
    next if /#/;
    my @F = split ' ';
-   $total += $F[1];
+   push @y, $F[1];
+   push @x, $F[0];
 }
-@ARGV = @input_files;
-while(<>) {
-   next if /#/;
-   my @F = split ' ';
-   printf "$F[0] %f\n", $F[1] / $total;
+#@ARGV = @input_files;
+#while(<>) {
+#   next if /#/;
+#   my @F = split ' ';
+#   printf "$F[0] %f\n", $F[1] / $total;
+#}
+for(@y) {
+   $total += $_;
+}
+for(0..$#y) {
+   printf "$x[$_] %f\n", $y[$_] / $total;
 }
