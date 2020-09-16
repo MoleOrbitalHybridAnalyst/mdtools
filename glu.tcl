8878d39683f837a4aa77be0ff3a7a9e6d8e306f2
MoleOrbitalHybridAnalyst <lch004218@gmail.com>
2020-09-15 22:40:06 -0500

    glu.tcl now moves glu to box center

--- glu.tcl
+++ glu.tcl
@@ -13,8 +13,24 @@ mol modselect 3 0 not protein and within 8 of (protein and sidechain and oxygen)
 mol modstyle 3 0 VDW
 mol selupdate 3 0 1
 mol addrep 0
-mol modselect 4 0 serial 445
+mol modselect 4 0 serial [expr [molinfo 0 get numatoms] + 1]
 display projection orthographic
 #color Display Background white
 #color Labels Bonds black
 source ~/share/vmdtools.tcl
+
+set nf [molinfo 0 get numframes]
+for {set i 0} {$i < $nf} {incr i} {
+   animate goto $i
+   set mm [measure minmax [atomselect 0 all]]
+   set xmean [expr ([lindex [lindex $mm 0] 0] + [lindex [lindex $mm 1] 0]) / 2]
+   set ymean [expr ([lindex [lindex $mm 0] 1] + [lindex [lindex $mm 1] 1]) / 2]
+   set zmean [expr ([lindex [lindex $mm 0] 2] + [lindex [lindex $mm 1] 2]) / 2]
+   set cen [geo_center [atomselect 0 "name OE OC"]]
+   set shift {}
+   lappend shift [expr $xmean - [lindex $cen 0]]
+   lappend shift [expr $ymean - [lindex $cen 1]]
+   lappend shift [expr $zmean - [lindex $cen 2]]
+   [atomselect 0 all] moveby $shift
+   pbc wrap
+}
