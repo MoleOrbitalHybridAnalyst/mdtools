80ad3c28e2e2b3c3190c78be05e30e949fb4e4eb
MoleOrbitalHybridAnalyst <lch004218@gmail.com>
2021-12-13 15:45:06 -0800

    add light speed

---
+++ constants.py
@@ -0,0 +1,26 @@
+# energy in kcal/mol
+kcal = 1
+kj = 0.23900573614
+hartree = 627.509608030
+eV = 23.060541945
+
+# lenght in A
+m = 1e10
+A = 1
+Bohr = 0.529177
+
+# time in ps
+s = 1e12
+ps = 1
+fs = 1e-3
+ns = 1e3
+us = 1e6
+
+# physical constants
+hbar = 0.01517872841498232  # in kcal/mol * ps
+kb = 0.001985875            # in kcal/mol/K
+NA = 6.0221409e+23          # in mol^-1
+c = 299792458 * m / s       # in A / ps
+
+# derived units
+au_t = hbar / hartree  # https://manual.cp2k.org/trunk/CP2K_INPUT/MOTION/PRINT/VELOCITIES.html
