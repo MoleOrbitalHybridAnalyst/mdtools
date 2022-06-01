c5978b37fb9b0ddd14477dc0d692bddc52ccf600
MoleOrbitalHybridAnalyst <lch004218@gmail.com>
2022-06-01 10:07:33 -0700

    update constants.py

--- constants.py
+++ constants.py
@@ -7,7 +7,7 @@ eV = 23.060541945
 # lenght in A
 m = 1e10
 A = 1
-Bohr = 0.529177
+Bohr = 0.529177249
 
 # time in ps
 s = 1e12
@@ -16,11 +16,17 @@ fs = 1e-3
 ns = 1e3
 us = 1e6
 
+NA = 6.0221409e+23                           # in mol^-1
+
+# mass in kcal/mol /A^2 * ps^2
+# 1 kg = 1e-3 kJ / (m^2/s^2) = 1e-3 kj/mol/NA / (m^2/s^2) * NA = 1e-3 kj/(m^2/s^2) * NA
+kg = 1e-3 * kj / (m**2/s**2) * NA 
+
 # physical constants
-hbar = 0.01517872841498232  # in kcal/mol * ps
-kb = 0.001985875            # in kcal/mol/K
-NA = 6.0221409e+23          # in mol^-1
-c = 299792458 * m / s       # in A / ps
+hbar = 0.01517872841498232                   # in kcal/mol * ps
+kb = 0.001985875                             # in kcal/mol/K
+c = 299792458 * m / s                        # in A / ps
+mp = 1.67262192369E-27 * kg                  # in kcal/mol / A^2 * ps^2
 
 # derived units
 au_t = hbar / hartree  # https://manual.cp2k.org/trunk/CP2K_INPUT/MOTION/PRINT/VELOCITIES.html
