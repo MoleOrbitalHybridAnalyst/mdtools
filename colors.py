a8b20fcb6c4411468276823f52467bebc5b3f606
MoleOrbitalHybridAnalyst <lch004218@gmail.com>
2022-07-16 15:22:58 -0700

    add colors.py

---
+++ colors.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+"""
+colors.py
+=========
+
+A set of colors and color maps as provided by python package `palettable` (https://jiffyclub.github.io/palettable).
+
+"""
+
+import numpy as np
+
+from palettable.colorbrewer.qualitative import Set1_9
+from palettable.tableau import Tableau_10, Tableau_20
+from cycler import cycler
+import matplotlib.pyplot as plt
+
+# Set some commonly used colors
+almost_black = '#262626'
+light_grey = np.array([float(248) / float(255)] * 3)
+
+# ColorBrewer
+# by Drs. Cynthia Brewer and Mark Harrower of Pennsylvania
+# State University. For more information on ColorBrewer, see:
+# - Flash-based interactive map:
+# http://colorbrewer2.org/
+# - A quick visual reference to every ColorBrewer scale:
+# http://bl.ocks.org/mbostock/5577023
+# https://jiffyclub.github.io/palettable/colorbrewer
+#
+# ColorBrewer scale 'Qualitative.Set1'.
+# This one has nice "traditional" colors like reds and blues
+# It is suitable for multi-line plots and category items.
+brewer_set1 = Set1_9.mpl_colors
+# Remove the sixth color (yellow) which is too bright
+brewer_set1.pop(5)
+# Swap the red and blue to let blue come first
+brewer_set1[0], brewer_set1[1] = brewer_set1[1], brewer_set1[0]
+# Add a decent black color to this list
+brewer_set1.append(almost_black)
+
+# Tableau 10 & 20 Classic
+# See: https://jiffyclub.github.io/palettable/tableau/
+# Another great qualitative set of colors suitable for multi-line plots.
+# 10 and 20 are number of colors in the list.
+tableau_10 = Tableau_10.mpl_colors
+# Add a decent black color
+tableau_10.append(almost_black)
+# Swap orange and red
+tableau_10[1], tableau_10[3] = tableau_10[3], tableau_10[1]
+# swap orange and purple
+# now table_au has similar sequence to brewer_set1
+tableau_10[3], tableau_10[4] = tableau_10[4], tableau_10[3]
+# This is 20-color Tableau which contains light version of tableau_10
+tableau_20 = Tableau_20.mpl_colors
+
+plt.rcParams['axes.prop_cycle'] = cycler('color', tableau_10)
+plt.rcParams['xtick.direction'] = 'out'
+plt.rcParams['ytick.direction'] = 'out'
+plt.rcParams["font.family"] = "stix"
+plt.rcParams["mathtext.fontset"] = "stix"
+
+def set_plt_scale(s=1):
+   plt.rcParams["figure.figsize"] = (24*s,18*s)
+   plt.rcParams["font.size"] = 85*s
+   plt.rcParams["axes.labelsize"] = 85*s
+   plt.rcParams['axes.linewidth'] = 5*s
+   plt.rcParams['lines.linewidth'] = 9*s
+   plt.rcParams['xtick.major.width'] = 7.5*s
+   plt.rcParams['ytick.major.width'] = 7.5*s
+   plt.rcParams['xtick.major.size'] = 20*s
+   plt.rcParams['ytick.major.size'] = 20*s
+   plt.rcParams['lines.markersize'] = 20*s
+   plt.rcParams['legend.fontsize'] = 50*s
+
+set_plt_scale(1.0)
