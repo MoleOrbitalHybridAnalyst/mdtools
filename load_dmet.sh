e90c78d04a26f1bf7704763a3de6b6c9e4fad6eb
MoleOrbitalHybridAnalyst <lch004218@gmail.com>
2022-08-24 18:55:06 -0700

    update load dmet

--- load_dmet.sh
+++ load_dmet.sh
@@ -1,2 +1,2 @@
 . ${HOME}/share/load_pyscf.sh
-export PYTHONPATH=/home/chhli/packages/dmet:/home/chhli/packages/libdmet_solid:$PYTHONPATH
+export PYTHONPATH=${HOME}/packages/DMET:${HOME}/packages/libdmet_solid:$PYTHONPATH
